7cb4f25fac721a79c5f97f332463a55d8794a51e
Toshiki Teramura <toshiki.teramura@gmail.com>
2015-03-14 19:16:33 +0900

    Remove template from TransitionMap

--- TransitionMap.hpp
+++ TransitionMap.hpp
@@ -21,17 +21,17 @@ namespace cgGraph {
  *    - StateP.{duration,count,prob,rate,entropy}
  *    - TransitP.{count,prob}
  */
-template <typename Int = unsigned long> class TransitionMap {
+class TransitionMap {
 public:
   TransitionMap();
 
   /** 状態のプロパティ
    * \headerfile TransitionMap.hpp "DS/cgGraph/TransitionMap.hpp" */
   struct StateP {
-    Int duration;   ///< 滞在時間の合計
-    Int count;      ///< 滞在した回数
-    double prob;    ///< 状態の不変確率測度
-    double entropy; ///< 遷移の不確実性
+    uint64_t duration; ///< 滞在時間の合計
+    uint64_t count;    ///< 滞在した回数
+    double prob;       ///< 状態の不変確率測度
+    double entropy;    ///< 遷移の不確実性
     ///  @f$ H_i = \sum_{j \neq i} w(i \to j) \log_2 w(i \to j) @f$
     double rate; ///< 遷移率 @f$ w_i = \sum_{j \neq i} w(i \to j) @f$
                  ///  (Poisson過程と仮定する)
@@ -41,35 +41,36 @@ public:
   /** 遷移のプロパティ
    * \headerfile TransitionMap.hpp "DS/cgGraph/TransitionMap.hpp" */
   struct TransitP {
-    Int count;
+    uint64_t count;
     double prob; ///< 遷移確率
   };
-  typedef std::tuple<Int, Int> Transit;
+  typedef std::tuple<uint64_t, uint64_t> Transit;
   typedef std::map<Transit, TransitP> TransitMap;
   typedef typename TransitMap::iterator TransitMapIterator;
 
   /** 遷移 (from, to) を登録する */
-  void push(Int from, Int to, Int stay_time /** fromに滞在した時間 */);
+  void push(uint64_t from, uint64_t to,
+            uint64_t stay_time /** fromに滞在した時間 */);
 
   /** 保持したカウントから各状態の存在確率とその間の遷移確率を計算する */
   void finalize();
   bool is_finalized() const;
 
   /** 登録された遷移の総数 */
-  Int transition_count() const;
+  uint64_t transition_count() const;
   /** 実現した (from, to) の組の個数 */
-  Int combination_count() const;
+  uint64_t combination_count() const;
   /** 経過時間 */
-  Int time() const;
+  uint64_t time() const;
 
   /** 状態の詳細を取得する */
   const StateList &get_states() const;
   /** 個々の状態の詳細を取得する */
-  const StateP &get_state(Int) const;
+  const StateP &get_state(uint64_t) const;
   /** 遷移の詳細を取得する */
   const TransitMap &get_transits() const;
   /** 個々の遷移の詳細を取得する */
-  const TransitP &get_transit(Int from, Int to) const;
+  const TransitP &get_transit(uint64_t from, uint64_t to) const;
 
 private:
   bool finalized_flag;
