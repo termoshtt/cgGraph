bd8d0c9c4d63d64c0734e6ab9d7b2ab6f03a04a1
Toshiki Teramura <toshiki.teramura@gmail.com>
2015-06-16 17:38:27 +0900

    Add basic functionalities

--- logger.cpp
+++ logger.cpp
@@ -1,13 +1,47 @@
 
 #include "logger.hpp"
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/utility/setup/file.hpp>
+
+namespace logging = boost::log;
+namespace attrs = logging::attributes;
+namespace keywords = logging::keywords;
 
 namespace cgGraph {
+
+Attrs get_default_attrs() {
+  return { { "time", "TimeStamp" },
+           { "tag", "Tag" },
+           { "run_id", "RunID" },
+           { "scope", "Scope" },
+           { "severity", "Severity" },
+           { "message", "Message" } };
+}
 
 void init() {
   auto &lg = logger::get();
   logging::add_common_attributes();
   lg.add_attribute("Tag", log::attrs::make_constant("cgGraph"));
   lg.add_attribute("Scope", log::attrs::named_scope());
+}
+
+void set_run_id(std::string id) {
+  auto &lg = logger::get();
+  lg.add_attribute("RunID", attrs::make_constant(id));
+}
+
+void add_file_log(std::string filename, const Attrs &attr, bool auto_flush) {
+  std::stringstream ss;
+  for (auto &&pair : attr) {
+    const std::string &key = pair.first;
+    const std::string &val = pair.second;
+    if (!ss.str().empty())
+      ss << "\t";
+    ss << key << ":%" << val << "%";
+  }
+  logging::add_file_log(keywords::file_name = filename,
+                        keywords::format = ss.str(),
+                        keywords::auto_flush = auto_flush);
 }
 
 } // namespace cgGraph
--- logger.hpp
+++ logger.hpp
@@ -17,10 +17,22 @@ namespace attrs = logging::attributes;
 namespace src = logging::sources;
 }
 
-using severity_level = logging::trivial::severity_level;
+using severity = logging::trivial::severity_level;
 
-BOOST_LOG_INLINE_GLOBAL_LOGGER_DEFAULT(
-    logger, log::src::severity_logger_mt<severity_level>);
+BOOST_LOG_INLINE_GLOBAL_LOGGER_DEFAULT(logger,
+                                       log::src::severity_logger_mt<severity>);
+BOOST_LOG_ATTRIBUTE_KEYWORD(line_id, "LineID", unsigned int)
+BOOST_LOG_ATTRIBUTE_KEYWORD(severity_attr, "Severity", severity)
+BOOST_LOG_ATTRIBUTE_KEYWORD(tag_attr, "Tag", std::string)
+BOOST_LOG_ATTRIBUTE_KEYWORD(run_id, "RunID", std::string)
+BOOST_LOG_ATTRIBUTE_KEYWORD(scope, "Scope",
+                            boost::log::attributes::named_scope::value_type)
+
+typedef std::vector<std::pair<std::string, std::string> > Attrs;
+Attrs get_default_attrs();
 void init();
+void set_run_id(std::string id);
+void add_file_log(std::string filename, const Attrs &attr = get_default_attrs(),
+                  bool auto_flush = false);
 
 } // namespace cgGraph
