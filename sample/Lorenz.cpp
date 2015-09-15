244c3f5211b60486ce5867b71413db6765492e4d
Toshiki Teramura <toshiki.teramura@gmail.com>
2015-09-15 20:06:22 +0900

    Revise output of Lorenz

--- sample/Lorenz.cpp
+++ sample/Lorenz.cpp
@@ -1,13 +1,9 @@
 
 #include "../SimpleCover.hpp"
-#include "../Timeline.hpp"
-#include "../algorithm.hpp"
-
+#include <yaml-cpp/yaml.h>
+#include <msgpack.hpp>
+#include <fstream>
 #include <iostream>
-#include <boost/lexical_cast.hpp>
-#include <boost/log/attributes/mutable_constant.hpp>
-
-namespace attrs = boost::log::attributes;
 
 const double p = 10.0;
 const double r = 28.0;
@@ -24,13 +20,18 @@ void Lorenz(std::vector<double> &v, double dt) {
 
 int main(int argc, char const *argv[]) {
   if (argc != 2) {
-    std::cerr << "Usage: " << argv[0] << " [radius > 0]" << std::endl;
+    std::cerr << "Usage: " << argv[0] << " [configure.yml]" << std::endl;
     return 1;
   }
 
+  auto cfg = YAML::LoadFile(argv[1]);
+  std::cout << cfg << std::endl;
+
   // parameters
-  double dt = 1.0e-3;
-  double r = atof(argv[1]);
+  double dt = cfg["dt"].as<double>();
+  double r = cfg["r"].as<double>();
+  uint64_t T = cfg["T"].as<uint64_t>();
+  uint64_t interval = cfg["interval"].as<uint64_t>();
   if (r <= 0) {
     std::cerr << "Invalid radius : " << r << " (<=0)" << std::endl;
     return 1;
@@ -40,22 +41,25 @@ int main(int argc, char const *argv[]) {
   std::vector<double> v = { 1, 0, 0 };
 
   // discards transient
-  for (unsigned int t = 0; t < 10000; t++) {
+  for (unsigned int t = 0; t < T / 100; t++) {
     Lorenz(v, dt);
   }
 
   // Ready for cgGraph
   cgGraph::SimpleCover c(3, r);
-  cgGraph::Timeline tl;
+  std::vector<uint64_t> tl;
 
   // Main
-  for (unsigned int t = 0; t < 10000000; t++) {
+  for (unsigned int t = 0; t < T; t++) {
     Lorenz(v, dt);
-    unsigned long idx = c.get_nearest(v);
-    tl.push_back(idx);
+    if (t % interval == 0) {
+      uint64_t idx = c.get_nearest(v);
+      tl.push_back(idx);
+    }
   }
-  auto tmap = cgGraph::count_transition(tl);
-  cgGraph::save_graph(tmap, "pp.bin", &c);
+
+  std::ofstream ofs("Lorenz.msg");
+  msgpack::pack(ofs, tl);
 
   return 0;
 }
